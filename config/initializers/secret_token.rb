7c1a97a7c2ad7c03313a7fdebd1b2a93ca6f3f7f
Azarel Doroteo Pacheco <azarel.doroteo@logicalbricks.com>
2012-04-06 16:28:03 -0500

    Created application using Refinery CMS 2.0.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TlacolulitaCom::Application.config.secret_token = '74e506da2d018d3e198993b83ad5eb6b471df52f4820d91f14b15e8b5d340471e0afbf52eff85d5cefd919cb7607f97e4b1cf4cfd4dc25f899bd9cf5b99de8f4'
